958e9f61fb6ca17b86d724c3a8f7dae02e9dee68
ENG : MARWAN <marwanashref40@gmail.com>
2025-06-30 05:16:55 +0300

    Add files via upload

---
+++ tempreture.py
@@ -0,0 +1,11 @@
+temp = int(input(' Enter the tempreture in Celsius : '))
+
+if temp >= 30 :
+    print (" It's a hot day. Stay hydrated! ")
+elif temp >= 20 and temp <= 29 :
+    print (" It's a warm day. Enjoy the weather! ")
+elif temp >= 10 and temp <= 19 :
+    print (" It's a cool day. Wear a jacket! ")
+else :
+    print (" It's a cold day. Stay warm! ")
+
